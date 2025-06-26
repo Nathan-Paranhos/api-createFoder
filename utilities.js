2bd081e5a217c3a97bf5bffa06f013662fe57867
Jean Vencigueri <jean.vencigueri@fagrontech.com.br>
2025-06-26 13:53:32 -0300

    Primeiro commit

---
+++ utilities.js
@@ -0,0 +1,16 @@
+(function(){
+    function validaSeExiste(componente){ //string da classe aqui ppfv
+        variavel = document.querySelectorAll(componente);
+        if(variavel){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    if(validaSeExiste('#form-kickoff')){
+        
+    }
+
+})()
